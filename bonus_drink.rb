1eaa756ab18f527c72ff42cf5e9b432c9e7362e8
root <root@LAPTOP-EMV2T81C.localdomain>
2018-05-27 18:18:39 +0900

    add answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,18 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+
+    if total <= 0
+      total = 0
+    end
+
+    loop do
+      if amount - 3 < 0 then
+        break
+      end
+     amount = amount - 3 + 1
+     total += 1
+    end
+    total
   end
 end
